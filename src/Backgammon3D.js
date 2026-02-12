c24944e76b89eed89b85f61b817e509b5d6412a9
onurakbaltr <onurakbal.tr@gmail.com>
2026-02-13 00:01:29 +0300

    Add files via upload

--- src/Backgammon3D.js
+++ src/Backgammon3D.js
@@ -80,7 +80,7 @@ export class Backgammon3D {
         const height = this.container.clientHeight;
         this.camera = new THREE.PerspectiveCamera(50, 0.5, 0.1, 100);
         this.camera.position.set(0, 100, 32);
-        this.camera.lookAt(0, 0, 2);
+        this.camera.lookAt(0, 0, 0);
 
         // ─── Renderer ───
         this.renderer = new THREE.WebGLRenderer({
